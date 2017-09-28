a4576ea0d9e9353fb09ae3054acf34ba0ff2a234
joshparrilla23 <josh.parrilla24@gmail.com>
2017-09-29 05:24:33 +0800

    Updating files and added uploader files

--- src/AppBundle/Controller/UploadController.php
+++ src/AppBundle/Controller/UploadController.php
@@ -2,18 +2,47 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\File;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\HttpFoundation\Request;
 
 class UploadController extends Controller
 {
     /**
-     * @Route("/upload")
+     * @Route("/upload", name="upload")
      */
-    public function uploadAction()
+    public function uploadAction(Request $request)
     {
+        $formfile = new File();
+        $form = $this->createForm(FileType::class, $formfile);
+        $formfile->setUser($this->getUser());
+        $form ->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+
+            $file = $formfile->getName();
+
+            $filename = md5(uniqid()).'.'.$file->guessExtension();
+
+            $file -> move(
+
+                $this->getParameter('File_Directory'),
+                $filename
+
+            );
+
+            $formfile->setName($file);
+
+            return $this->redirect($this->generateUrl('file_upload'));
+
+
+        }
+
         return $this->render('AppBundle:Upload:upload.html.twig', array(
             // ...
+            'form' => $form->createView(),
         ));
     }
 
