bd6bb5934b77a67c65587c2dfa9c170b6c05014c
joshparrilla23 <josh.parrilla24@gmail.com>
2017-09-30 17:22:12 +0800

    Sorted the query to find values that are only members from the admin side

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -52,7 +52,11 @@ class DefaultController extends Controller
 
         $userdata = $this->getDoctrine()
                ->getRepository(User::class)
-                ->findAll();
+                ->findBy([
+
+                    'userrole' => 'member'
+
+                ]);
 
 
 
